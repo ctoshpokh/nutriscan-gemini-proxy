7a1cb139e3c5713910a4a38acf82da040d86ad42
Sitosh Pokhrel <spcrazzyguy@gmail.com>
2026-06-05 12:38:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('https');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
 
 const PORT = process.env.PORT || 10000;
 const API_KEY = process.env.GEMINI_API_KEY;
